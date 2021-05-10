acb5405e0b2b77ef3128a26cb11712c173ebf951
pdong2001 <79776591+pdong2001@users.noreply.github.com>
2021-05-10 12:17:28 +0700

    fix padding

--- resources/js/functions.js
+++ resources/js/functions.js
@@ -4,5 +4,26 @@ $(document).ready(function() {
     }, function() {
         $(this).find('.drop-down-content').slideUp('fast');
     });
+
     document.documentElement.style.setProperty('--height', $('.slide .product').height() + 'px');
+
+    $(window).scroll(function() {
+        var wT = $(this).scrollTop();
+        var mT = $('#main-menu').height() + 120;
+        if (wT > mT) {
+            $('#main-menu').css({
+                'position': 'fixed',
+                'background-color': 'white',
+                'top': '0px',
+                'width': '100%'
+            });
+        } else {
+            $('#main-menu').css({
+                'position': 'relative',
+                'background-color': 'white',
+                'top': '0px',
+                'width': '100%'
+            });
+        }
+    })
 });
